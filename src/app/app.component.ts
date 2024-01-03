224b7bc93f143246cc021c0665f8a5a61900e401
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-01-03 19:50:02 +0100

    Mobile interface.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,42 @@ import { Component, ElementRef, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarmobileComponent } from './navbarmobile/navbarmobile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, NavbarmobileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  // host: {
-  //   '(keydown)': 'updateValue($event)',
-  // },
+  host: {
+    '(window:resize)': 'onWindowResize($event)',
+    '(document:keydown)': 'handleKeyboardEvent($event)',
+  },
 })
 export class AppComponent {
-  // In the docs they ask not to use @HostListener
-  // https://angular.dev/guide/components/host-elements#the-hostbinding-and-hostlistener-decorators
+  // https://angular.dev/guide/components/host-elements
 
-  constructor(private router: Router, private elementRef: ElementRef) {}
+  // This part is to control if either the normal navbar or the navbarmobile shoulf be loaded
+  // See also the host up
+  public screenWidth!: number;
 
-  @HostListener('document:keydown', ['$event'])
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+  }
+  
+  onWindowResize() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  isMobile(screenWidth: number){
+    return screenWidth < 700
+  }
+
+  // This part is to control the behaviour of the shortcuts
+  // See also the host up
+  constructor(private router: Router) {}
+
   handleKeyboardEvent(event: KeyboardEvent) {
     switch (event.key) {
       case 'q':
--- src/app/navbarmobile/navbarmobile.component.ts
+++ src/app/navbarmobile/navbarmobile.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbarmobile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbarmobile.component.html',
   styleUrl: './navbarmobile.component.css'
 })
 export class NavbarmobileComponent {
 
+  navbarVisible: boolean = false;
+
+  public onClick(){
+    this.navbarVisible = !this.navbarVisible;
+  }
+
+// This part down is to highlight the menu item
+  currentRoute!: string;
+  
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd)
+        this.currentRoute = this.router.url.toString();
+    });
+  }
+
+  isRouteActive(linkRoute: string) {
+    if (linkRoute === this.currentRoute) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
